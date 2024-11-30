8d575bb54a727fb6e8dce07ea9193a176acf5060
Dawool23 <aladko23@gmail.com>
2024-11-30 12:10:56 +0700

    Admin panel

--- manager_motivation/motivation/admin.py
+++ manager_motivation/motivation/admin.py
@@ -3,13 +3,89 @@ from django.conf import settings
 from .models import *
 
 class RolesAdmin(admin.ModelAdmin):
-    list_display =(
+    list_display = (
         'id',
         'name',
         'fillial')
     
     list_display_links = ('id', 'name', 'fillial',)
 
-    search_fields = ('id', 'institute__name__icontains','name',)
+    search_fields = (
+        )
 
 admin.site.register(Roles, RolesAdmin)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'fio',
+        'id_roles')
+    
+    list_display_links = (
+        'id',
+        'user',
+        'fio',
+        'id_roles')
+
+    search_fields = ()
+
+admin.site.register(Employee, EmployeeAdmin)
+
+class ClientsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',)
+    
+    list_display_links = (
+        'id',
+        'name',)
+
+    search_fields = ()
+
+admin.site.register(Clients, ClientsAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'price',
+        'percent',
+        'description',)
+    
+    list_display_links = (
+        'id',
+        'name',
+        'price',
+        'percent',
+        'description',)
+
+    search_fields = ()
+
+admin.site.register(Product, ProductAdmin)
+
+class DealsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'employers_id',
+        'client_id',
+        'date',
+        'product_id',
+        'payment_type',
+        'sum',
+        'status',)
+    
+    list_display_links = (
+        'id',
+        'employers_id',
+        'client_id',
+        'date',
+        'product_id',
+        'payment_type',
+        'sum',
+        'status',)
+
+    search_fields = ()
+
+admin.site.register(Deals, DealsAdmin)
+
--- manager_motivation/motivation/models.py
+++ manager_motivation/motivation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from djmoney.models.fields import MoneyField
+from datetime import datetime
 
 
 class Roles(models.Model):
@@ -10,7 +11,7 @@ class Roles(models.Model):
 
     class Meta:
         db_table = 'roles'
-        verbose_name = 'Роли'
+        verbose_name = 'Роль'
         verbose_name_plural = 'Роли'
 
     def __str__(self):
@@ -20,12 +21,11 @@ class Employee(models.Model):
     id = models.AutoField(primary_key=True, db_column='id')
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     fio = models.CharField(max_length=150, db_column='FIO', verbose_name='ФИО')
-    mail = models.CharField(max_length=100, db_column='mail', verbose_name='Email')
     id_roles = models.ForeignKey(Roles, on_delete=models.CASCADE, db_column='id_roles', verbose_name='Роль')
 
     class Meta:
         db_table = 'employee'
-        verbose_name = 'Сотрудники'
+        verbose_name = 'Сотрудника'
         verbose_name_plural = 'Сотрудники'
 
     def __str__(self):
@@ -37,7 +37,7 @@ class Clients(models.Model):
 
     class Meta:
         db_table = 'clients'
-        verbose_name = 'Клиенты'
+        verbose_name = 'Клиента'
         verbose_name_plural = 'Клиенты'
 
     def __str__(self):
@@ -48,47 +48,42 @@ class Product(models.Model):
     name = models.CharField(max_length=50, db_column='name', verbose_name='Название товара')
     price = MoneyField(max_digits=12, decimal_places=2, default_currency='RUB', db_column='price', verbose_name='Стоимость')
     percent = models.DecimalField(max_digits=4, decimal_places=2, db_column='percent', verbose_name='Процент')
-    description = models.CharField(max_length=300, db_column='description', verbose_name='Описание')
+    description = models.CharField(max_length=300, db_column='description', verbose_name='Описание', null=True, blank=True)
 
     class Meta:
         db_table = 'product'
-        verbose_name = 'Товвр'
-        verbose_name_plural = 'Товар'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
     def __str__(self):
         return self.name
 
 class Deals(models.Model):
+    STATUS_CHOICES = [
+        ('Не завершено', 'Не завершено'),
+        ('Завершено', 'Завершено'),
+        ('Отклонено', 'Отклонено'),
+    ]
+
+    PAYMENT_TYPE_CHOICES = [
+        ('Наличные', 'Наличные'),
+        ('Банковская карта', 'Банковская карта'),
+        ('Кредит', 'Кредит'),
+    ]
+
     id = models.AutoField(primary_key=True, db_column='id')
     employers_id = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column='employers_id', verbose_name='Сотрудник')
     client_id = models.ForeignKey(Clients, on_delete=models.CASCADE, db_column='client_id', verbose_name='Клиент')
-    date =  models.DateTimeField(db_column='date', verbose_name='Дата')
+    date =  models.DateTimeField(db_column='date', verbose_name='Дата', default=datetime.now)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id', verbose_name='Товар')
-    payment_type = models.CharField(max_length=50, db_column='payment_type', verbose_name='Тип оплаты')
+    payment_type = models.CharField(max_length=50, choices=PAYMENT_TYPE_CHOICES, db_column='payment_type', verbose_name='Тип оплаты', default='Наличные')
     sum = MoneyField(max_digits=12, decimal_places=2, default_currency='RUB', db_column='sum', verbose_name='Сумма')
-    status = models.CharField(max_length=50, db_column='status', verbose_name='Статус')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, db_column='status', verbose_name='Статус', default='Не завершено')
 
     class Meta:
         db_table = 'deals'
-        verbose_name = 'Сделки'
+        verbose_name = 'Сделку'
         verbose_name_plural = 'Сделки'
 
     def __str__(self):
         return self.employers_id
-    
-""" class Payment(models.model):
-    id = models.AutoField(primary_key=True, db_column='id')
-    deals_id = models.ForeignKey(Deals, on_delete=models.CASCADE, db_column='id', verbose_name='Сделка')
-    payment_sum = MoneyField(max_digits=12, decimal_places=2, default_currency='RUB', db_column='payment_sum', verbose_name='Сумма оплаты')
-    status = models.CharField(max_length=50, db_column='status', verbose_name='Статус')
-    type = models.CharField(max_length=50, db_column='type', verbose_name='Тип оплаты')
-    date =  models.DateTimeField(db_column='date', verbose_name='Дата')
-
-    class Meta:
-        db_table = 'payment'
-        verbose_name = 'Оплата'
-        verbose_name_plural = 'Оплата'
-
-    def __str__(self):
-        return self.payment_sum """
-
