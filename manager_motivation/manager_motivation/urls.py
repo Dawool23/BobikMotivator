24d4a949f8bc8bab9eef7ce04dc180e8655b7165
Яцик Валерий <darkyacik@yandex.ru>
2024-11-30 14:44:42 +0700

    logout

--- manager_motivation/manager_motivation/urls.py
+++ manager_motivation/manager_motivation/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from motivation.views import LoginAPIView
+from motivation.views import LoginAPIView, LogoutAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/login/', LoginAPIView.as_view(), name='login'),
+    path('api/logout/', LogoutAPIView.as_view(), name='logout'),
 ]
