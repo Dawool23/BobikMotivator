cf7c8b0215e25ca78ee1aa8c9b16095b8bb9ff83
Yorik8877 <darkyacik@yandex.ru>
2024-11-30 23:34:40 +0700

    url

--- manager_motivation/manager_motivation/urls.py
+++ manager_motivation/manager_motivation/urls.py
@@ -6,8 +6,8 @@ from motivation.views import LoginAPIView, RolesAPIView, EmployeeAPIView, Client
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Авторизация
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/login/', LoginAPIView.as_view(), name='login'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login', LoginAPIView.as_view(), name='login'),
     path('api/roles', RolesAPIView.as_view(), name='roles'),
     path('api/clients', ClientsAPIView.as_view(), name='clients'),
     path('api/product', ProductAPIView.as_view(), name='product'),
