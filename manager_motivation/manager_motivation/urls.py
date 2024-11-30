6402a51053426524e87a9a91574c50cd0249d844
Яцик Валерий <darkyacik@yandex.ru>
2024-11-30 13:51:28 +0700

    BACK-routes login, logout

--- manager_motivation/manager_motivation/urls.py
+++ manager_motivation/manager_motivation/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from motivation.views import LoginAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login/', LoginAPIView.as_view(), name='login'),
 ]
--- manager_motivation/motivation/views.py
+++ manager_motivation/motivation/views.py
@@ -1,1 +1,76 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+
+class Profile(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+
+        data = request.data
+
+        username = data.get('username', None)
+
+        password = data.get('password', None)
+
+        if username is None or password is None:
+
+            return Response({'error': 'Нужен и логин, и пароль'},
+
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+
+            return Response({'error': 'Неверные данные'},
+
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+
+        refresh.payload.update({
+
+            'user_id': user.id,
+
+            'username': user.username
+
+        })
+
+        return Response({
+
+            'refresh': str(refresh),
+
+            'access': str(refresh.access_token),
+
+        }, status=status.HTTP_200_OK)
+
+class LogoutAPIView(APIView):
+
+    def post(self, request):
+        refresh_token = request.data.get('refresh_token') # С клиента нужно отправить refresh token
+
+        if not refresh_token:
+            return Response({'error': 'Необходим Refresh token'},
+
+                            status=status.HTTP_400_BAD_REQUEST)
+        try:
+
+            token = RefreshToken(refresh_token)
+            token.blacklist() # Добавить его в чёрный список
+
+        except Exception as e:
+
+            return Response({'error': 'Неверный Refresh token'},
+
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'success': 'Выход успешен'}, status=status.HTTP_200_OK)
+    
